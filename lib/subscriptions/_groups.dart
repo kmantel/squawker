aee604363d1446597592d738c43757409e7dbda7
Harley <123662124+TheHCJ@users.noreply.github.com>
2023-05-23 20:54:24 +0100

    _groups.dart didn't commit last time :(

--- lib/subscriptions/_groups.dart
+++ lib/subscriptions/_groups.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:flutter_iconpicker_plus/IconPicker/Packs/Material.dart';
 import 'package:flutter_iconpicker_plus/flutter_iconpicker.dart';
 import 'package:flutter_triple/flutter_triple.dart';
 import 'package:quacker/constants.dart';
@@ -206,6 +207,9 @@ class _SubscriptionGroupEditDialogState extends State<SubscriptionGroupEditDialo
       return const Center(child: CircularProgressIndicator());
     }
 
+    // Filter the Material icons to only the ones the app uses
+    var iconPack = icons.entries.where((value) => value.key.endsWith('_outlined') || value.key.endsWith('_outline'));
+
     return AlertDialog(
       actions: [
         TextButton(
@@ -316,7 +320,8 @@ class _SubscriptionGroupEditDialogState extends State<SubscriptionGroupEditDialo
                     icon: Icon(deserializeIconData(icon)),
                     onPressed: () async {
                       var selectedIcon = await FlutterIconPicker.showIconPicker(context,
-                          iconPackModes: [IconPack.material],
+                          iconPackModes: [IconPack.custom],
+                          customIconPack: Map.fromEntries(iconPack),
                           title: Text(L10n.of(context).pick_an_icon),
                           closeChild: Text(L10n.of(context).close),
                           searchHintText: L10n.of(context).search,
