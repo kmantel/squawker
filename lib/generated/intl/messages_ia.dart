0775385bab0cf835f1ac8e9f175465d7c1fd4ccc
jfbriere <jeanfrancois.briere@creativaxion.com>
2024-01-24 20:19:02 -0500

    add one generated language to sync to github

---
+++ lib/generated/intl/messages_ia.dart
@@ -0,0 +1,73 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ia locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ia';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "add_to_group": MessageLookupByLibrary.simpleMessage("Adder al gruppo"),
+        "could_not_find_any_tweets_by_this_user":
+            MessageLookupByLibrary.simpleMessage(
+                "Non poteva trovar necun tweet de iste usator!"),
+        "could_not_find_any_tweets_from_the_last_7_days":
+            MessageLookupByLibrary.simpleMessage(
+                "Non poteva trovar tweets ab le ultime 7 dies!"),
+        "feed": MessageLookupByLibrary.simpleMessage("Fluxos"),
+        "filters": MessageLookupByLibrary.simpleMessage("Filtros"),
+        "no_results": MessageLookupByLibrary.simpleMessage("Nulle resultatos"),
+        "reporting_an_error":
+            MessageLookupByLibrary.simpleMessage("Signalar un error"),
+        "saved": MessageLookupByLibrary.simpleMessage("Salvate"),
+        "select": MessageLookupByLibrary.simpleMessage("Seliger"),
+        "subscribe": MessageLookupByLibrary.simpleMessage("Subscriber se"),
+        "subscriptions": MessageLookupByLibrary.simpleMessage("Subscriptiones"),
+        "this_group_contains_no_subscriptions":
+            MessageLookupByLibrary.simpleMessage(
+                "Iste gruppo non ha subscriptiones!"),
+        "trending": MessageLookupByLibrary.simpleMessage("Tendentias"),
+        "unable_to_find_your_saved_tweets":
+            MessageLookupByLibrary.simpleMessage(
+                "Non poteva trovar tu tweets salvate."),
+        "unable_to_load_subscription_groups":
+            MessageLookupByLibrary.simpleMessage(
+                "Non poteva cargar le gruppos de subscription"),
+        "unable_to_load_the_group":
+            MessageLookupByLibrary.simpleMessage("Non poteva cargar le gruppo"),
+        "unable_to_load_the_group_settings":
+            MessageLookupByLibrary.simpleMessage(
+                "Non poteva cargar le configurationes de gruppo"),
+        "unable_to_load_the_next_page_of_replies":
+            MessageLookupByLibrary.simpleMessage(
+                "Non poteva cargar le sequente pagina de responsas"),
+        "unable_to_load_the_next_page_of_tweets":
+            MessageLookupByLibrary.simpleMessage(
+                "Non poteva cargar le sequente pagina de tweets"),
+        "unable_to_load_the_search_results":
+            MessageLookupByLibrary.simpleMessage(
+                "Non poteva cargar le resultatos de recerca."),
+        "unable_to_load_the_tweet":
+            MessageLookupByLibrary.simpleMessage("Non poteva cargar le tweet"),
+        "unable_to_load_the_tweets_for_the_feed":
+            MessageLookupByLibrary.simpleMessage(
+                "Non poteva cargar le tweets pro le fluxo"),
+        "unsubscribe":
+            MessageLookupByLibrary.simpleMessage("Cancellar le subscription")
+      };
+}
