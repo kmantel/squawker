3555c8395de7ad06e99c667a8648b860895258ec
jfbriere <jeanfrancois.briere@creativaxion.com>
2024-02-22 15:44:42 -0500

    add Vietnamese (only generated to be in sync).

---
+++ lib/generated/intl/messages_vi.dart
@@ -0,0 +1,173 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a vi locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'vi';
+
+  static String m1(fileName) => "Xuất dữ liệu đến ${fileName}";
+
+  static String m2(fullPath) => "Xuất dữ liệu đến ${fullPath}";
+
+  static String m8(date) => "Đã tham gia từ ${date}";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "add_to_group": MessageLookupByLibrary.simpleMessage("Thêm vào nhóm"),
+        "app_info": MessageLookupByLibrary.simpleMessage("Thông tin ứng dụng"),
+        "beta": MessageLookupByLibrary.simpleMessage("Thử nghiệm"),
+        "cancel": MessageLookupByLibrary.simpleMessage("Hủy"),
+        "could_not_find_any_tweets_by_this_user":
+            MessageLookupByLibrary.simpleMessage(
+                "Không tìm thấy bất kỳ tweet nào của người dùng này!"),
+        "could_not_find_any_tweets_from_the_last_7_days":
+            MessageLookupByLibrary.simpleMessage(
+                "Không tìm thấy bất kỳ tweet nào trong 7 ngày qua!"),
+        "dark": MessageLookupByLibrary.simpleMessage("Tối"),
+        "data": MessageLookupByLibrary.simpleMessage("Dữ liệu"),
+        "data_exported_to_fileName": m1,
+        "data_exported_to_fullPath": m2,
+        "data_imported_successfully":
+            MessageLookupByLibrary.simpleMessage("Nhập dữ liệu thành công"),
+        "default_tab": MessageLookupByLibrary.simpleMessage("Thẻ mặc định"),
+        "disabled": MessageLookupByLibrary.simpleMessage("Từ chối"),
+        "export": MessageLookupByLibrary.simpleMessage("Xuất"),
+        "export_guest_accounts":
+            MessageLookupByLibrary.simpleMessage("Xuất các tài khoản gợi ý?"),
+        "export_settings":
+            MessageLookupByLibrary.simpleMessage("Cài đặt xuất dữ liệu?"),
+        "export_subscription_group_members":
+            MessageLookupByLibrary.simpleMessage(
+                "Xuất thành viên của các nhóm đăng ký?"),
+        "export_subscription_groups":
+            MessageLookupByLibrary.simpleMessage("Xuất các nhóm đăng ký?"),
+        "export_subscriptions":
+            MessageLookupByLibrary.simpleMessage("Xuất các đăng ký?"),
+        "export_tweets":
+            MessageLookupByLibrary.simpleMessage("Xuất các tweet?"),
+        "feed": MessageLookupByLibrary.simpleMessage("Nguồn tin"),
+        "filters": MessageLookupByLibrary.simpleMessage("Bộ lọc"),
+        "followers": MessageLookupByLibrary.simpleMessage("Người theo dõi"),
+        "following": MessageLookupByLibrary.simpleMessage("Đang theo dõi"),
+        "general": MessageLookupByLibrary.simpleMessage("Chung"),
+        "import": MessageLookupByLibrary.simpleMessage("Nhập"),
+        "import_data_from_another_device":
+            MessageLookupByLibrary.simpleMessage("Nhập từ thiết bị khác"),
+        "include_replies":
+            MessageLookupByLibrary.simpleMessage("Bao gồm câu trả lời"),
+        "include_retweets":
+            MessageLookupByLibrary.simpleMessage("Bao gồm đăng lại"),
+        "joined": m8,
+        "large": MessageLookupByLibrary.simpleMessage("Lớn"),
+        "legacy_android_import":
+            MessageLookupByLibrary.simpleMessage("Nhập Legacy Android"),
+        "light": MessageLookupByLibrary.simpleMessage("Sáng"),
+        "material_3": MessageLookupByLibrary.simpleMessage("Dùng Material 3?"),
+        "media": MessageLookupByLibrary.simpleMessage("Phương tiện"),
+        "media_size":
+            MessageLookupByLibrary.simpleMessage("Kích thước phương tiện"),
+        "medium": MessageLookupByLibrary.simpleMessage("Vừa"),
+        "no_results": MessageLookupByLibrary.simpleMessage("Không có kết quả"),
+        "note_due_to_a_twitter_limitation_not_all_tweets_may_be_included":
+            MessageLookupByLibrary.simpleMessage(
+                "Lưu ý: Do giới hạn của Twitter/X, không phải tất cả các tweet đều có thể được đưa vào"),
+        "reporting_an_error": MessageLookupByLibrary.simpleMessage("Báo lỗi"),
+        "save_bandwidth_using_smaller_images":
+            MessageLookupByLibrary.simpleMessage(
+                "Tiết kiệm lưu lượng mạng với kích thước nhỏ hơn"),
+        "saved": MessageLookupByLibrary.simpleMessage("Đã lưu"),
+        "select": MessageLookupByLibrary.simpleMessage("Chọn"),
+        "send": MessageLookupByLibrary.simpleMessage("Gửi"),
+        "settings": MessageLookupByLibrary.simpleMessage("Cài đặt"),
+        "small": MessageLookupByLibrary.simpleMessage("Nhỏ"),
+        "something_just_went_wrong_in_fritter_and_an_error_report_has_been_generated":
+            MessageLookupByLibrary.simpleMessage(
+                "Đã xảy ra lỗi trên Squawker và đã tạo báo cáo lỗi . Báo cáo có thể được gửi đến các nhà phát triển Squawker để khắc phục sự cố."),
+        "subscribe": MessageLookupByLibrary.simpleMessage("Dịch"),
+        "subscriptions": MessageLookupByLibrary.simpleMessage("Đăng ký"),
+        "system": MessageLookupByLibrary.simpleMessage("Hệ thống"),
+        "thanks_for_helping_fritter": MessageLookupByLibrary.simpleMessage(
+            "Cảm ơn vì đã giúp đỡ Squawker! 💖"),
+        "theme": MessageLookupByLibrary.simpleMessage("Chủ đề"),
+        "theme_mode": MessageLookupByLibrary.simpleMessage("Giao diện chủ đề"),
+        "this_group_contains_no_subscriptions":
+            MessageLookupByLibrary.simpleMessage(
+                "Nhóm này không có mục đăng ký nào!"),
+        "this_user_does_not_follow_anyone":
+            MessageLookupByLibrary.simpleMessage(
+                "Người dùng không theo dõi ai!"),
+        "this_user_does_not_have_anyone_following_them":
+            MessageLookupByLibrary.simpleMessage(
+                "Người dùng này không có ai theo dõi!"),
+        "thumbnail": MessageLookupByLibrary.simpleMessage("Hình thu nhỏ"),
+        "trending": MessageLookupByLibrary.simpleMessage("Xu hướng"),
+        "true_black": MessageLookupByLibrary.simpleMessage("Đen?"),
+        "tweets": MessageLookupByLibrary.simpleMessage("Tweets"),
+        "tweets_and_replies":
+            MessageLookupByLibrary.simpleMessage("Tweets & Trả lời"),
+        "unable_to_find_your_saved_tweets":
+            MessageLookupByLibrary.simpleMessage(
+                "Không tìm thấy các tweet đã lưu"),
+        "unable_to_load_subscription_groups":
+            MessageLookupByLibrary.simpleMessage("Không thể tải nhóm đăng ký"),
+        "unable_to_load_the_group":
+            MessageLookupByLibrary.simpleMessage("Không thể tải nhóm"),
+        "unable_to_load_the_group_settings":
+            MessageLookupByLibrary.simpleMessage("Không thể tải cài đặt nhóm"),
+        "unable_to_load_the_list_of_follows":
+            MessageLookupByLibrary.simpleMessage(
+                "Không thể tải danh sách theo dõi"),
+        "unable_to_load_the_next_page_of_follows":
+            MessageLookupByLibrary.simpleMessage(
+                "Không thể tải danh sách theo dõi kế tiếp"),
+        "unable_to_load_the_next_page_of_replies":
+            MessageLookupByLibrary.simpleMessage(
+                "Không thể tải trang trả lời kế tiếp"),
+        "unable_to_load_the_next_page_of_tweets":
+            MessageLookupByLibrary.simpleMessage(
+                "Không thể tải tweet tiếp theo"),
+        "unable_to_load_the_profile":
+            MessageLookupByLibrary.simpleMessage("Không thể tải hồ sơ"),
+        "unable_to_load_the_search_results":
+            MessageLookupByLibrary.simpleMessage(
+                "Không thể tải kết quả tìm kiếm"),
+        "unable_to_load_the_tweet":
+            MessageLookupByLibrary.simpleMessage("Không thể tải tweet"),
+        "unable_to_load_the_tweets":
+            MessageLookupByLibrary.simpleMessage("Không thể tải các tweet"),
+        "unable_to_load_the_tweets_for_the_feed":
+            MessageLookupByLibrary.simpleMessage(
+                "Không thể tải tweet cho nguồn tin"),
+        "unsubscribe": MessageLookupByLibrary.simpleMessage("Hủy đăng ký"),
+        "use_true_black_for_the_dark_mode_theme":
+            MessageLookupByLibrary.simpleMessage(
+                "Ở chế độ tối, dùng màu đen thay vì màu xám"),
+        "which_tab_is_shown_when_the_app_opens":
+            MessageLookupByLibrary.simpleMessage(
+                "Thẻ nào sẽ hiển thị ra khi mở ứng dụng"),
+        "would_you_like_to_enable_automatic_error_reporting":
+            MessageLookupByLibrary.simpleMessage(
+                "Bạn có muốn bật tự động báo lỗi không?"),
+        "you_have_not_saved_any_tweets_yet":
+            MessageLookupByLibrary.simpleMessage(
+                "Bạn chưa lưu bất kỳ tweet nào!"),
+        "your_report_will_be_sent_to_fritter__project":
+            MessageLookupByLibrary.simpleMessage(
+                "Báo cáo của bạn sẽ được gửi đến Squawker và bạn có thể tìm thấy chi tiết về quyền riêng tư tại:")
+      };
+}
